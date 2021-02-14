e98d03aac1479aac2be764fc29cb22a1db7f5724
N0FreeLunch <consilience90@gmail.com>
2021-02-14 17:58:02 +0900

    laravel default auth

--- README.md
+++ README.md
@@ -1,9 +1,45 @@
 ## laravel
 ```
 composer install
+php artisan migrate
 ```
 
 ## make sql file
 ```
 touch database/database.sqlite
+```
+
+## default authenticate scaffolding
+```
+composer require laravel/ui
+```
+
+```
+// Generate basic scaffolding...
+php artisan ui bootstrap
+php artisan ui vue
+php artisan ui react
+
+// Generate login / registration scaffolding...
+php artisan ui bootstrap --auth
+php artisan ui vue --auth
+php artisan ui react --auth
+```
+
+## use react scaffolding
+```
+php artisan ui react --auth
+```
+> [auth/login.blade.php]
+> [auth/passwords/confirm.blade.php]
+> [auth/passwords/email.blade.php]
+> [auth/passwords/reset.blade.php]
+> [auth/register.blade.php]
+> [auth/verify.blade.php]
+> [home.blade.php]
+> [layouts/app.blade.php]
+> [HomeController.php]
+
+```
+npm install
 ```
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// page
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// session API
+Route::post('/test', function () {
+    return response() -> json([
+      "test" => true
+    ]);
+});
+
+Route::post('/login', function () {
+    return response() -> json([
+      "test" => true
+    ]);
+});
+
+Route::group(['middleware' => ['auth']], function () {
+  Route::post('/auth', function () {
+      return response() -> json([
+        "auth" => true
+      ]);
+  });
+});
+
+
+// default
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
